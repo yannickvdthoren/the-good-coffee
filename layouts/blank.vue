24b3601b5c3d039045d6cabb51af497268f27932
Yannick Vandenthoren <yannickvdthoren@gmail.com>
2023-11-10 12:08:16 +0100

    Add links page for Instagram

---
+++ layouts/blank.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <slot></slot>
+  </main>
+</template>
+<script>
+export default {
+  name: "Layout default",
+};
+</script>
+<style>
+.wrap {
+  overflow-x: hidden;
+  max-width: 650px;
+  margin: 0 auto;
+  padding: 0 var(--wrap-margin);
+  min-height: 100vh;
+}
+.wrap::before {
+  content: "";
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin-left: var(--full-bleed);
+  background: inherit;
+  z-index: -1;
+}
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+}
+@media screen and (min-width: 1761px) {
+  .wrap {
+    overflow-x: visible;
+  }
+}
+</style>
---
+++ pages/links.vue
@@ -0,0 +1,104 @@
+<template>
+  <Title> Links | The Good Coffee </Title>
+  <NuxtLayout name="blank">
+    <section>
+      <div class="details">
+        <TextParagraph fontSize="var(--md)" fontFamily="var(--sans-serif)">
+          Hey 👋 <br />
+          Moi, c'est Yannick.
+        </TextParagraph>
+        <TextParagraph
+          fontSize="var(--md)"
+          fontFamily="var(--sans-serif)"
+          fontWeight="400"
+        >
+          Je suis passionné de café et je suis en train de travailler sur le
+          lancement d'une plateforme de vente de café éthique et de qualité.
+        </TextParagraph>
+        <TextParagraph
+          fontSize="var(--md)"
+          fontFamily="var(--sans-serif)"
+          fontWeight="400"
+        >
+          Tu peux retrouver mes liens ci-dessous. <br />À la prochaine !
+        </TextParagraph>
+      </div>
+      <ul class="links">
+        <li>
+          <ContentList :path="'/'" :sort="[{ date: -1 }]" v-slot="{ list }">
+            <LinkPrimary
+              v-for="article in list.slice(0, 1)"
+              :key="article._path"
+              :link="article._path"
+              type="nuxt"
+            >
+              {{ article.title }}
+            </LinkPrimary>
+          </ContentList>
+        </li>
+        <li>
+          <LinkPrimary link="https://market.the-good-coffee.com/">
+            Marketplace
+          </LinkPrimary>
+        </li>
+        <li>
+          <LinkPrimary link="/" type="nuxt"> The good coffee </LinkPrimary>
+        </li>
+        <li>
+          <LinkPrimary link="https://the-good-coffee-places.com/">
+            The good coffee places
+          </LinkPrimary>
+        </li>
+      </ul>
+      <MenuSocial class="social" />
+      <LogoPrimary class="logo" />
+    </section>
+  </NuxtLayout>
+</template>
+<script>
+export default {
+  name: "links",
+};
+</script>
+<style scoped>
+section {
+  display: grid;
+  grid-template-areas: "title" "links" "links" "social" "logo";
+  gap: 16px;
+  align-content: space-between;
+  justify-items: center;
+  min-height: 100vh;
+  padding: 32px 0;
+}
+.details {
+  grid-area: title;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+.links {
+  grid-area: links;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+}
+.links a {
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  padding: 12px 0;
+  color: var(--white);
+  background: var(--green);
+  border-radius: 50px;
+}
+.social {
+  grid-area: social;
+}
+.logo {
+  grid-area: logo;
+  max-width: 150px;
+  height: auto;
+}
+</style>
